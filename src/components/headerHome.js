572e6b64491615caf2198a64898dd42cb7ef1bed
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-05-24 07:48:22 -0400

    navbar

--- src/components/headerHome.js
+++ src/components/headerHome.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useLayoutEffect } from "react"
 import { Link } from "gatsby"
 import { debounce } from "../utilities/helpers"
 import logo from "../assets/images/logo2.svg"
