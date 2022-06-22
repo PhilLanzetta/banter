35c276bd1ece2f641e4f36a3766a020fbb7dfc47
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2022-06-22 16:41:54 -0400

    add delay to marquee

--- src/components/marquee.js
+++ src/components/marquee.js
@@ -10,11 +10,17 @@ const MarqueeSlider = () => {
       }
     }
   `)
+
+  const delay =
+    typeof sessionStorage.getItem("welcomeShown") !== "undefined" &&
+    sessionStorage.getItem("welcomeShown") !== "true"
+
   return (
     <Marquee
       className="word-marquee"
       gradient={false}
       speed={80}
+      delay={delay ? 6.5 : 0}
     >
       {[...Array(10)].map((e, i) => (
         <div className="marquee-items" key={i}>
